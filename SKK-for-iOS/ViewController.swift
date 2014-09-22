10123743fd795cd181347e2081366208ce0c8ef9
mzp <mzpppp@gmail.com>
2014-09-22 23:35:55 +0900

    Initial Commit

---
+++ SKK-for-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SKK-for-iOS
+//
+//  Created by mzp on 2014/09/22.
+//  Copyright (c) 2014年 codefirst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
